752324f97ab86307ead806888eb070d23f2533e9
Abhishek Gujamagdi <61365651+Abhigujamagdi@users.noreply.github.com>
2021-03-18 14:41:17 -0700

    76 ms

---
+++ solutions/852. Peak Index in a Mountain Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        for i, values in enumerate(arr):
+            return arr.index(max(arr))
+            
+            
+        
